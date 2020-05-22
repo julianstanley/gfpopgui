e8212c32ab2e020ca71f8e1c358af602e2131f4f
julianstanley <stanley.ju@husky.neu.edu>
2020-05-22 15:30:19 -0400

    commented out some unsafe lines of start script

--- dev/01_start.R
+++ dev/01_start.R
@@ -15,12 +15,12 @@
 ## Add meta data about your application
 golem::fill_desc(
   pkg_name = "gfpopgui", # The Name of the package containing the App 
-  pkg_title = "PKG_TITLE", # The Title of the package containing the App 
-  pkg_description = "PKG_DESC.", # The Description of the package containing the App 
-  author_first_name = "AUTHOR_FIRST", # Your First Name
-  author_last_name = "AUTHOR_LAST", # Your Last Name
-  author_email = "AUTHOR@MAIL.COM", # Your Email
-  repo_url = NULL # The URL of the GitHub Repo (optional) 
+  pkg_title = "gfpop GUI", # The Title of the package containing the App 
+  pkg_description = "A Shiny-based graphical user interface (GUI) for the gfpop package.", # The Description of the package containing the App 
+  author_first_name = "Julian", # Your First Name
+  author_last_name = "Stanley", # Your Last Name
+  author_email = "julianstanleya@gmail.com", # Your Email
+  repo_url = "https://github.com/julianstanley/gfpop-gui" # The URL of the GitHub Repo (optional) 
 )     
 
 ## Set {golem} options ----
@@ -28,14 +28,14 @@ golem::set_golem_options()
 
 ## Create Common Files ----
 ## See ?usethis for more information
-usethis::use_mit_license( name = "Golem User" )  # You can set another license here
+usethis::use_mit_license( name = "Julian Stanley" )  # You can set another license here
 usethis::use_readme_rmd( open = FALSE )
 usethis::use_code_of_conduct()
 usethis::use_lifecycle_badge( "Experimental" )
 usethis::use_news_md( open = FALSE )
 
 ## Use git ----
-usethis::use_git()
+# usethis::use_git()
 
 ## Init Testing Infrastructure ----
 ## Create a template for tests
