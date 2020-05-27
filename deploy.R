b2b732ba162b6343ee30ae23743552dba1a2bca2
julianstanley <stanley.ju@husky.neu.edu>
2020-05-27 19:39:47 -0400

    more troubleshooting

--- deploy.R
+++ deploy.R
@@ -1,6 +1,4 @@
-pkgload::load_all(export_all = FALSE,helpers = FALSE,attach_testthat = FALSE)
-options( "golem.app.prod" = TRUE)
-
+#!/usr/bin/env Rscript
 setAccountInfo(
   name = Sys.getenv("shinyapps_name"),
   token = Sys.getenv("shinyapps_token"),
