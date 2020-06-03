1f84fd480dbe9d7d608a1fcaf6c245b5d3779cec
Julian Stanley <julianst@mit.edu>
2020-06-03 13:01:45 -0400

    Cleaning up tests (#28)
    A bunch of troubleshooting to clean up tests. Most notably, I disabled the shinyapps.io tests on CI, since they were inconsistently passing/failing. But they can still be run locally.
    
    * Cleaning up tests
    
    * navbar title should be gfpop
    
    * Remove screenshot from top level
    
    * Do local tests work on CI now?
    
    * Screenshots make travis check fail
    
    * Maybe try_again can help tests pass?
    
    * More cleanup. Maybe now we don't have to sleep before shinyapp query?
    
    * Add Sys.sleep back in
    
    * non-functional change to test whether travis tests are consistent here
    
    * skip remote tests on ci
    
    * another non-functional change to check consistency

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,14 +1,4 @@
 library(testthat)
 library(gfpopgui)
 
-# Run the following before running local tests: --------------------------------
-# Start RSelenium server
-# system("docker run -d --net=host selenium/standalone-firefox&")
-# ------------------------------------------------------------------------------
-
-print("In testthat, starting shiny in background")
-system("${R_HOME}/bin/Rscript -e 'library(gfpopgui);options(shiny.port = 11616);run_app()' &")
-# Give the system a couple seconds to load app
-Sys.sleep(2)
-
 test_check("gfpopgui")
---
+++ tests/testthat/test-integration-tests.R
@@ -0,0 +1,65 @@
+library(RSelenium)
+library(testthat)
+
+# Initialization steps ---------------------------------------------------------
+
+# For local debug, change to true!
+SCREENSHOT <- FALSE
+RUN_DOCKER <- FALSE
+
+# Start a local selenium server via docker (local only)
+if(RUN_DOCKER) {
+  system("docker run -d --net=host selenium/standalone-firefox&")
+}
+
+# Open port to RSelenium server
+remDr <- remoteDriver(remoteServerAddr = "localhost", port = 4444, browser = "firefox") 
+remDr$open(silent = TRUE)
+
+# Kill the shiny application, if it's running
+# Use port=15123 for testing
+init.kill.app <- system("lsof -i:15123 -t | xargs kill", 
+                        ignore.stdout = TRUE,
+                        ignore.stderr = TRUE)
+
+# Start the shiny application
+system("${R_HOME}/bin/Rscript -e 'library(gfpopgui);options(shiny.port = 15123);run_app()' &", 
+       ignore.stdout = TRUE,
+       ignore.stderr = TRUE)
+
+# Give the system a few seconds to load app
+Sys.sleep(4)
+
+# Local Tests ------------------------------------------------------------------
+
+# Connect to the local app, wait for a second for load
+remDr$navigate(url = "http://127.0.0.1:15123")
+
+try_again(5, test_that("can connect to app, local", {
+  appTitle <- remDr$getTitle()[[1]]
+  # Screenshot debug, local only:
+  if(SCREENSHOT) {
+    remDr$screenshot(file = "screenshots/test_connect_local.png")
+  }
+  expect_equal(appTitle, "gfpopgui")
+}))
+
+# Remote Tests -----------------------------------------------------------------
+try_again(5, test_that("can connect to app, remote", {
+  skip_on_ci()
+  # Connect to the remote app, wait a second for load
+  remDr$navigate(url = "http://julianstanley.shinyapps.io/gfpopgui")
+  Sys.sleep(2)
+  appTitle <- remDr$getTitle()[[1]]
+  expect_equal(appTitle, "gfpopgui")
+}))
+
+# Cleanup steps ----------------------------------------------------------------
+
+# Close selenium server
+remDr$close()
+
+# Kill app
+clean.kill.app <- system("lsof -i:15123 -t | xargs kill", intern = TRUE)
+
+
