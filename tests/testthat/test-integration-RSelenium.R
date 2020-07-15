f9e0b557d3d11da1de822365278a300b46d0d23b
julianstanley <stanley.ju@husky.neu.edu>
2020-07-15 10:16:23 -0400

    Added more integration testing (merge from `more-int-test` branch)

--- tests/testthat/test-integration-RSelenium.R
+++ tests/testthat/test-integration-RSelenium.R
@@ -2,104 +2,148 @@ library(RSelenium)
 library(testthat)
 enable_integration_tests <- FALSE
 if (enable_integration_tests) {
-user <- "julianstanley"
-pass <- "0135a1f4-8837-4202-a428-d58151760e6b"
-port <- 80
-ip <- paste0(user, ":", pass, "@ondemand.saucelabs.com")
-rdBrowser <- "chrome"
-version <- "latest"
-platform <- "Windows 10"
-
-extraCapabilities <- list(
-  name = "Main test-integration",
-  username = user,
-  accessKey = pass,
-  tags = list("RSelenium-vignette", "OS/Browsers-vignette"),
-  "screen-resolution" = "1920x1080"
-)
-
-remDr <- remoteDriver$new(
-  remoteServerAddr = ip,
-  port = port,
-  browserName = rdBrowser,
-  version = version,
-  platform = platform,
-  extraCapabilities = extraCapabilities
-)
-
-# Set build
-buildName <- paste0("build-gfpopgui-main_", 
-                    format(Sys.time(), "%m-%d-%Y_%s"))
-
-submit_job_info <- function(remDr, build, name, result) {
-  if (!(result %in% c("passed", "failed", "true", "false"))) {
-    stop("Invalid result. Please use: passed, failed, true, or false")
-  }
+  user <- "julianstanley"
+  pass <- "0135a1f4-8837-4202-a428-d58151760e6b"
+  port <- 80
+  ip <- paste0(user, ":", pass, "@ondemand.saucelabs.com")
+  rdBrowser <- "chrome"
+  version <- "latest"
+  platform <- "Windows 10"
   
-  remDr$executeScript(paste0("sauce:job-build=", build))
-  remDr$executeScript(paste0("sauce:job-name=", name))
-  remDr$executeScript(paste0("sauce:job-result=", result))
+  extraCapabilities <- list(
+    name = "Main test-integration",
+    username = user,
+    accessKey = pass,
+    tags = list("RSelenium-vignette", "OS/Browsers-vignette"),
+    "screen-resolution" = "2560x1600"
+  )
   
-} 
-
-is.bad <- function(code) {
-  isTRUE(tryCatch(code,
-                  error = function(c) TRUE,
-                  warning = function(c) TRUE
-  ))
-}
-
-wait_for_title <- function(remDr, seconds, title_expected) {
-  count <- 0
-  while(count < seconds) {
-    appTitle <- remDr$getTitle()[[1]]
-    if(appTitle == title_expected) {
-      break
+  remDr <- remoteDriver$new(
+    remoteServerAddr = ip,
+    port = port,
+    browserName = rdBrowser,
+    version = version,
+    platform = platform,
+    extraCapabilities = extraCapabilities
+  )
+  
+  # Set build
+  buildName <- paste0("build-gfpopgui-main_", 
+                      format(Sys.time(), "%m-%d-%Y_%s"))
+  
+  submit_job_info <- function(remDr, build, name, result) {
+    if (!(result %in% c("passed", "failed", "true", "false"))) {
+      stop("Invalid result. Please use: passed, failed, true, or false")
     }
-    message(paste0("Waited ", count, " seconds. App isn't loaded yet, waiting
+    
+    remDr$executeScript(paste0("sauce:job-build=", build))
+    remDr$executeScript(paste0("sauce:job-name=", name))
+    remDr$executeScript(paste0("sauce:job-result=", result))
+    
+  } 
+  
+  is.bad <- function(code) {
+    isTRUE(tryCatch(code,
+                    error = function(c) TRUE,
+                    warning = function(c) TRUE
+    ))
+  }
+  
+  wait_for_title <- function(remDr, seconds, title_expected) {
+    count <- 0
+    while(count < seconds) {
+      appTitle <- remDr$getTitle()[[1]]
+      if(appTitle == title_expected) {
+        break
+      }
+      message(paste0("Waited ", count, " seconds. App isn't loaded yet, waiting
                    for another second."))
-    Sys.sleep(1)
-    count <- count + 1
+      Sys.sleep(1)
+      count <- count + 1
+    }
+    
+    return(appTitle)
   }
   
-  return(appTitle)
-}
-
-test_that("can connect to app", {
-  remDr$open(silent = T)
-  remDr$navigate(url = "http://julianstanley.shinyapps.io/gfpopgui")
-  remDr$setImplicitWaitTimeout(milliseconds = 5000)
-  appTitle <- wait_for_title(remDr, 10, "gfpopgui")
-  result <- if(appTitle == "gfpopgui") "true" else "false"
-  submit_job_info(remDr, buildName, name = "can connect to app",
-                                    result = result)
-  expect_equal(appTitle, "gfpopgui")
-  remDr$close()
-})
-
-test_that("the generate data button works", {
-  remDr$open(silent = T)
-  remDr$navigate(url = "http://julianstanley.shinyapps.io/gfpopgui")
-  remDr$setImplicitWaitTimeout(milliseconds = 5000)
-  # Wait for the app to load
-  appTitle <- wait_for_title(remDr, 10, "gfpopgui")
-  if(appTitle != "gfpopgui") {
-    warning("Title is not 'gfpopgui'. Did the application load successfully?")
-  }
-  # Select entry from DataTable 0 to make sure it exists
-  webElem <- remDr$findElement("xpath", "//table[@id='DataTables_Table_0']/tbody/tr/td[2]")
-  result1 <- webElem$getElementAttribute("innerHTML")[[1]] == "Std"
-  expect_equal(webElem$getElementAttribute("innerHTML")[[1]], "Std")
+  test_that("can connect to app", {
+    remDr$open(silent = T)
+    remDr$navigate(url = "http://julianstanley.shinyapps.io/gfpopgui")
+    remDr$setImplicitWaitTimeout(milliseconds = 5000)
+    appTitle <- wait_for_title(remDr, 10, "gfpopgui")
+    result <- if(appTitle == "gfpopgui") "true" else "false"
+    submit_job_info(remDr, buildName, name = "can connect to app",
+                    result = result)
+    expect_equal(appTitle, "gfpopgui")
+    remDr$close()
+  })
   
-  expect_error(remDr$findElement("xpath", "//table[@id='DataTables_Table_1']/tbody/tr/td[2]"))
-  remDr$findElement("id", "home_ui_1-genData")$clickElement()
-  webElem <- remDr$findElement("xpath", "//table[@id='DataTables_Table_1']/tbody/tr/td[2]")
-  result2 <- webElem$getElementAttribute("innerHTML")[[1]] == "1"
-  expect_equal(webElem$getElementAttribute("innerHTML")[[1]], "1")
+  test_that("the generate data button works", {
+    remDr$open(silent = T)
+    remDr$navigate(url = "http://julianstanley.shinyapps.io/gfpopgui")
+    remDr$setImplicitWaitTimeout(milliseconds = 5000)
+    # Wait for the app to load
+    appTitle <- wait_for_title(remDr, 10, "gfpopgui")
+    if(appTitle != "gfpopgui") {
+      warning("Title is not 'gfpopgui'. Did the application load successfully?")
+    }
+    # Select entry from DataTable 0 to make sure it exists
+    webElem <- remDr$findElement("xpath", "//table[@id='DataTables_Table_0']/tbody/tr/td[2]")
+    result1 <- webElem$getElementAttribute("innerHTML")[[1]] == "Std"
+    expect_equal(webElem$getElementAttribute("innerHTML")[[1]], "Std")
+    
+    expect_error(remDr$findElement("xpath", "//table[@id='DataTables_Table_1']/tbody/tr/td[2]"))
+    remDr$findElement("id", "home_ui_1-genData")$clickElement()
+    webElem <- remDr$findElement("xpath", "//table[@id='DataTables_Table_1']/tbody/tr/td[2]")
+    result2 <- webElem$getElementAttribute("innerHTML")[[1]] == "1"
+    expect_equal(webElem$getElementAttribute("innerHTML")[[1]], "1")
+    
+    result <- if(result1 & result2) "true" else "false"
+    submit_job_info(remDr, buildName, name = "the generate data button works",
+                    result = result)
+    remDr$close()
+  })
   
-  result <- if(result1 & result2) "true" else "false"
-  submit_job_info(remDr, buildName, name = "the generate data button works",
-                                    result = result)
-  remDr$close()
-})
+  test_that("Can change a penalty value", {
+    remDr$open(silent = T)
+    remDr$navigate(url = "http://julianstanley.shinyapps.io/gfpopgui")
+    remDr$setImplicitWaitTimeout(milliseconds = 5000)
+    # Wait for the app to load
+    appTitle <- wait_for_title(remDr, 10, "gfpopgui")
+    if(appTitle != "gfpopgui") {
+      warning("Title is not 'gfpopgui'. Did the application load successfully?")
+    }
+    
+    # Initalize array of results
+    results <- c()
+    
+    # Generate some data and move to the analysis tab
+    remDr$findElement("id", "home_ui_1-genData")$clickElement()
+    remDr$findElement("link", "Analysis")$clickElement()
+    
+    # Ensure that the penalty starts at 15
+    webElem <- remDr$findElement("xpath", "//table[@id='DataTables_Table_2']/tbody/tr[2]/td[6]")
+    webElem_attr <- webElem$getElementAttribute("innerHTML")[[1]]
+    result <-  webElem_attr == "15"
+    results <- c(results, result)
+    expect_equal(webElem_attr, "15")
+    
+    # Change the penalty to 150
+    webElem <- remDr$findElement("id", "analysis_ui_1-pen")
+    webElem$sendKeysToElement(list(key="delete"))
+    webElem$sendKeysToElement(list(key="delete"))
+    webElem$sendKeysToElement(list("150"))
+    remDr$findElement("id", "analysis_ui_1-updateGraph")$clickElement()
+    
+    # Now ensure the penalty is 150
+    webElem <- remDr$findElement("xpath", "//table[@id='DataTables_Table_3']/tbody/tr[2]/td[6]")
+    webElem_attr <- webElem$getElementAttribute("innerHTML")[[1]]
+    result <-  webElem_attr == "150"
+    results <- c(results, result)
+    expect_equal(webElem_attr, "150")
+    
+    result <- if(all(results)) "true" else "false"
+    submit_job_info(remDr, buildName, name = "Can change a penalty value",
+                    result = result)
+    remDr$close()
+  })
 }
