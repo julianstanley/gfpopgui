48ec3e3261e078e758bc39788c3b53b6760d0191
julianstanley <stanley.ju@husky.neu.edu>
2020-08-26 10:19:08 -0400

    Try a different version of golem to fix remote testing problem

--- tests/testthat/test-golem-recommended.R
+++ tests/testthat/test-golem-recommended.R
@@ -1,11 +1,9 @@
 test_that("app ui", {
-  skip_if_not(interactive())
   ui <- app_ui()
   golem::expect_shinytaglist(ui)
 })
 
 test_that("app server", {
-  skip_if_not(interactive())
   server <- app_server
   expect_is(server, "function")
 })
